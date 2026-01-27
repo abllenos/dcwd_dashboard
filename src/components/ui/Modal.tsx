82eebcfaa5a2a4ea1a5361e6a11260f014fc271e
ZyleDoctolero <drzyle@gmail.com>
2026-01-27 13:46:44 +0800

    chore: empty branch

--- src/components/ui/Modal.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Modal as AntModal, ModalProps } from "antd";
-
-export const Modal: React.FC<ModalProps> = (props) => <AntModal {...props} />;
-
-export default Modal;
